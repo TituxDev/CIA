7c80f225137554902d123f14875142d1f5a76a0d
TituxDev <ocrstmy@gmail.com>
2025-03-14 11:16:10 -0600

    Create file secuence

--- codes/CIA.c
+++ codes/CIA.c
@@ -57,6 +57,7 @@ float ( *function[2][2] )( float )={
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 struct perceptron_t{
 	int inputs ;
 	int function ;
@@ -78,19 +79,24 @@ struct test_t{
 	float **in ;
 	float *out ;
 } ;
+
 void load_net( struct net_t ** , char * ) ;
 void load_test_data( char * , struct test_t ** , int , int ) ;
 void close_net( struct net_t * , char * ) ;
 void free_test( struct test_t * , int ) ;
 void execute( struct net_t * ) ;
-int train( struct net_t * , struct test_t ,  float , float , float ) ;
+int train( struct net_t * , struct test_t ,  float , float , int ) ;
 void print_net( struct net_t * , struct test_t , int ) ;
+void create_topo( struct net_t **) ;
+void create_boolean_test( struct test_t ** , int ) ;
 
 int main( int argc , char **argv ){
 	struct test_t *data ;
 	struct net_t *net ;
-	load_net( &net , argv[1] ) ;
-	load_test_data( argv[2] , &data , net->inputs , 4 ) ;
+	if( argc <= 2 ) create_topo( &net );
+	else load_net( &net , argv[1] ) ;
+	if( argc <= 3 ) create_boolean_test( &data , net->inputs );
+	else load_test_data( argv[2] , &data , net->inputs , 4 ) ;
 	printf( "\nMUESTRA:" ) ;
 	for( int i= 0 ; i < data->samples ; i++ ){
 		putchar( '\n' ) ;
@@ -126,6 +132,17 @@ void close_net( struct net_t *net , char *name ){
 	save_net( net , name ) ;
 	free_net( net ) ;
 }
+void create_boolean_test( struct test_t **data , int inputs ){
+	*data= malloc( sizeof( struct test_t ) ) ;
+	( *data )->samples= pow( 2 , inputs ) ;
+	( *data )->in= malloc( ( *data )->samples * sizeof( float * ) ) ;
+	( *data )->out= malloc( ( *data )->samples * sizeof( float ) ) ;
+	for( int i= 0 ; i < ( *data )->samples ; i++ ){
+		( *data )->in[i]= malloc( inputs * sizeof( float ) ) ;
+		for( int j= 0 ; j < inputs ; j++ )  ( *data )->in[i][j]= !!( i & ( 1 << j ) ) ;
+		( *data )->out[i]=  ( *data )->in[i][0] != ( *data )->in[i][1] ; // Output operation
+	}
+}
 void load_test_data( char *file , struct test_t **data , int inputs , int samples){
 	*data= malloc( sizeof( struct test_t ) ) ;
 	FILE *f ;
@@ -133,12 +150,15 @@ void load_test_data( char *file , struct test_t **data , int inputs , int sample
 	( *data )->in= malloc( ( *data )->samples * sizeof( float * ) ) ;
 	( *data )->out= malloc( ( *data )->samples * sizeof( float ) ) ;
 	f= fopen( file , "r" ) ;
-	for( int i= 0 ; i < ( *data )->samples ; i++ ){
-		( *data )->in[i]= malloc( inputs * sizeof( float ) ) ;
-		for( int j= 0 ; j < inputs ; j++ ) fscanf( f , "%f" , &( *data )->in[i][j] ) ;
-		fscanf( f , "%f" , &( *data )->out[i] ) ;
+	if( f == NULL ) create_boolean_test( data , inputs ) ;
+	else {
+		for( int i= 0 ; i < ( *data )->samples ; i++ ){
+			( *data )->in[i]= malloc( inputs * sizeof( float ) ) ;
+			for( int j= 0 ; j < inputs ; j++ ) fscanf( f , "%f" , &( *data )->in[i][j] ) ;
+			fscanf( f , "%f" , &( *data )->out[i] ) ;
+		}
+		fclose( f ) ;
 	}
-	fclose( f ) ;
 }
 void free_test( struct test_t *data , int inputs ){
 	for( int i= 0 ; i < inputs ; i++ ) free( data->in[i] ) ;
@@ -163,29 +183,40 @@ void print_net( struct net_t *net , struct test_t data , int decimals ){
 		}
 	}
 }
-float bk_error( struct perceptron_t * , float * , int , int ) ;
+// Función para normalizar un valor al rango (-1,1) sin incluir los extremos
+float normalize_value(float value) {
+	const float epsilon = 1e-6; // Pequeña constante para evitar los valores extremos
+	return (value > 0) ? value - epsilon : value + epsilon;
+}
+float bk_error( struct perceptron_t * , float * , int ) ;
 float derivate( struct perceptron_t * ) ;
 void adjust( struct perceptron_t * , float * , float , int ) ;
-int train( struct net_t *net , struct test_t data ,  float learning_rate , float tolerance , float max_attemps ){
+float calc_error( struct perceptron_t * , float , int , float * ) ;
+int train( struct net_t *net , struct test_t data ,  float learning_rate , float tolerance , int max_attemps ){
 	int counter= 0 ;
-	float error ;
 	int ly= net->layers - 1;
+	float errors ;
+	float delta_[net->topo[0]] ;
+	float back ;
 	do{
-		error= 0 ;
+		errors= 0 ;
 		for( int i= 0 ; i < data.samples ; i++ ){
 			for( int j= 0; j < net->inputs ; j++ ) net->in[j]= data.in[i][j] ;
 			execute( net ) ;
-			for( int j= 0 ; j < net->topo[ly] ; j++ ) error= fmax(fabs( data.out[i] - net->neuron[ly][j].out ) , error ) ;
-			if ( error < tolerance ) continue ;
-			float delta_[net->topo[0]] ;
-			float back ;
-			for( int j= ly ; j >= 0 ; j-- ){
-				for( int k= 0 ; k < net->topo[j + ( j < ly )] ; k++ ) delta_[k]= derivate( ( struct perceptron_t * )&net->neuron[j][k] ) * ( j == ly ? data.out[i] - net->neuron[j][k].out : back );
-				adjust( ( struct perceptron_t * )net->neuron[j] , delta_ , learning_rate ,net->topo[j] ) ;
-				back= j ? bk_error( ( struct perceptron_t * )net->neuron[j] , delta_ , net->topo[j - 1] , net->topo[j] ) : 0 ;
+			errors= fmax( errors , calc_error( ( struct perceptron_t * )net->neuron[ly] , data.out[i] ,net->topo[ly] , delta_) ) ;
+			if ( errors < tolerance ) continue ;
+			for( int j= 0 ; j < net->topo[ly] ; j++ ) delta_[j]*= derivate( ( struct perceptron_t * )&net->neuron[ly][j] ) ;
+			adjust( ( struct perceptron_t * )net->neuron[ly] , delta_ , learning_rate , net->topo[ly] ) ;
+			back= bk_error( (struct perceptron_t * )net->neuron[ly] , delta_ , net->topo[ly] ) ;
+ 			for( int j= ly - 1 ; j > 0 ; j-- ){
+				 for( int k= 0 ; k < net->topo[j] ; k++ ) delta_[k]= derivate( ( struct perceptron_t * )&net->neuron[j][k] ) * back ;
+				 adjust( ( struct perceptron_t * )net->neuron[j] , delta_ , learning_rate ,net->topo[j] ) ;
+				 back= bk_error( ( struct perceptron_t * )net->neuron[j] , delta_ , net->topo[j] ) ;
 			}
+			for( int j= 0 ; j < net->topo[0] ; j++ ) delta_[j]= derivate( ( struct perceptron_t * )&net->neuron[0][j] ) * back ;
+			adjust( ( struct perceptron_t * )net->neuron[0] , delta_ , learning_rate , net->topo[0] ) ;
 		}
-	} while( error > tolerance && ++counter < max_attemps ) ;
+	} while( errors > tolerance && ++counter < max_attemps ) ;
 	return counter ;
 }
 
@@ -200,10 +231,10 @@ float evaluate( struct perceptron_t *perceptron ){
 void execute( struct net_t *net  ){
 	for( int i= 0 ; i < net->layers ; i++ ) for( int j= 0 ; j < net->topo[i] ; j++ ) evaluate( &net->neuron[i][j] ) ;
 }
-float bk_error( struct perceptron_t *layer , float *delta_ , int prev_layer , int neurons ){
+float calc_error( struct perceptron_t *layer , float out , int neurons , float *error ){
 	float b= 0 ;
-	for( int i= 0 ; i < prev_layer ; i ++ ) for( int j= 0 ; j < neurons ; j++ ) b+= delta_[i] * layer[j].weight[i] ;
-	return b ;
+	for( int i= 0 ; i < neurons ; i++ ) b= fmax( b ,fabs( error[i]= out - layer[i].out) );
+	return b;
 }
 float derivate( struct perceptron_t *neuron ){
 	return neuron->fun[1]( neuron->out ) ;
@@ -214,6 +245,12 @@ void adjust( struct perceptron_t *neuron , float *delta_ , float learning_rate ,
 		neuron[k].bias+= delta_[k] * learning_rate ;
 	}	
 }
+float bk_error( struct perceptron_t *layer , float *delta_ , int neurons ){
+	float b= 0 ;
+	for( int j= 0 ; j < neurons ; j++ ) for( int i= 0 ; i < layer[j].inputs ; i ++ ) b+= delta_[i] * layer[j].weight[i] ;
+	return b ;
+}
+
 float boolean( float sum ){
 	return sum >= 0 ;
 }
@@ -227,40 +264,109 @@ float sigmoid_d( float sig ){
 	return sig * ( 1 - sig ) ;
 }
 
+void randomize_net(struct net_t **net , int a , int b ) {
+    for (int i = 0; i < (*net)->layers; i++) {
+        for (int j = 0; j < (*net)->topo[i]; j++) {
+            for (int k = 0; k < (*net)->neuron[i][j].inputs; k++) (*net)->neuron[i][j].weight[k] = ((float)((rand( ) % 10)) / 10) * ( (rand() % 2 ) ? 1 : -1) ;
+            float raw_bias = ((float)rand() / RAND_MAX) * 2.0 - 1.0;
+            (*net)->neuron[i][j].bias = normalize_value(raw_bias);
+        }
+    }
+}
+void create_topo( struct net_t **net ){
+	char O ;
+	printf( "\n\tDesea crear una nueva red neuronal? S / N : " ) ;
+	while( 1 ){
+		scanf( "%c" , &O ) ;
+		fflush( stdin ) ;
+		O&= ~32 ;
+		if( ( O ) == ( 's' & 'S' ) ) break ;
+		if( ( O ) == ( 'n' & 'N' ) ) exit( 0 ) ;
+		printf( "Opcion %c invalida, selecione una opcion ( S / N ) : " , O ) ; 
+	}
+	*net= malloc( sizeof( struct net_t ) ) ;
+	printf( "\n\tCantidad de entradas : " ) ;
+	scanf( "%i" , &( *net )->inputs ) ;
+	fflush( stdin ) ;
+	printf( "\n\tCantidad de capas : " ) ;
+	scanf( "%i" , &( *net )->layers ) ;
+	fflush( stdin ) ;
+	( *net )->in= malloc( ( *net )->inputs * sizeof( float ) ) ;
+	( *net )->neuron= malloc( ( *net )->layers * sizeof( struct perceptron_t * ) ) ;
+	( *net )->topo= malloc( ( *net )->layers * sizeof( float ) ) ;
+	printf( "\n\tCrear una red automatica? S / N : " ) ;
+	while( 1 ){
+		scanf( " %c" , &O ) ;
+		fflush( stdin ) ;
+		O&= ~32 ;
+		if( ( O ) == ( 's' & 'S' ) || O == ( 'n' & 'N' ) ) break ;
+		printf( "\n\tOpcion %c invalida, seleccione u a opcion ( S / N ) : " , O ) ;
+	}
+	if ( O == ( 'n' & 'N' ) ){
+		for( int i= 0 ; i < ( *net )->layers ; i++ ){
+			printf( "\n\tCantidad de neuronas en la capa %i : " , i + 1 ) ;
+			scanf( "%i" , &( *net )->topo[i] ) ;
+			fflush( stdin ) ;
+		}
+	}
+	if( O == ( 's' & 'S' ) ) for( int i= 0 , insize= ( *net )->inputs ; i < ( *net )->layers ; i++ ) ( *net )->topo[i]= ( *net )->layers - i ;
+	for( int i= 0 , insize= ( *net )->inputs ; i < ( *net )->layers ; i++ ){
+		( *net )->neuron[i]= malloc( ( *net )->topo[i] * sizeof( struct perceptron_t ) ) ;
+		for( int j= 0 ; j < ( *net )->topo[i] ; j++ ){
+			( *net )->neuron[i][j].inputs= insize ;
+			( *net )->neuron[i][j].in= malloc( ( *net )->neuron[i][j].inputs *  sizeof( float  * )) ;
+			for( int k= 0 ; k < ( *net )->neuron[i][j].inputs ; k++ ) ( *net )->neuron[i][j].in[k]= i ? &( *net )->neuron[i - 1][k].out : &( *net )->in[k] ;
+			( *net )->neuron[i][j].weight = malloc( ( *net )->neuron[i][j].inputs * sizeof( float )) ;
+		}
+		insize= ( *net )->topo[i] ;
+	}
+}
 void load_topo( char *file , struct net_t **net  ){
 	*net= malloc( sizeof( struct net_t ) ) ;
 	FILE *s ;
 	s= fopen( file , "r" ) ;
-	fscanf( s , "%i" , &( *net )->inputs ) ;
-	fscanf( s , "%i" , &( *net )->layers ) ;
-	( *net )->topo= malloc( ( *net )->layers * sizeof( int ) ) ;
-	for( int i= 0 ; i < ( *net )->layers ; i++ ) fscanf( s , "%i" , &( *net )->topo[i] ) ;
-	fclose( s ) ;
-	( *net )->in= malloc( ( *net )->inputs * sizeof( float ) ) ;
-	( *net )->neuron= malloc( ( *net )->layers * sizeof( struct perceptron_t * ) ) ;
-	for( int i= 0 , insize= ( *net )->inputs ; i < ( *net )->layers ; i++ ) {
-		fscanf( s , "%i" , &( *net )->topo[i] ) ;
-		( *net )->neuron[i]= malloc( ( *net )->topo[i] * sizeof( struct perceptron_t ) ) ;
-		for( int j= 0 ; j < ( *net )->topo[i] ; j++ ){
-			( *net )->neuron[i][j].inputs= insize ;
-			( *net )->neuron[i][j].in= malloc( ( *net )->neuron[i][j].inputs * sizeof( float * ) ) ;
-			for( int k= 0 ; k < ( *net )->neuron[i][j].inputs ; k++ ) ( *net )->neuron[i][j].in[k]= i ? &( *net )->neuron[i - 1][k].out : &( *net )->in[k] ;
-			( *net )->neuron[i][j].weight= malloc( ( *net )->neuron[i][j].inputs * sizeof( float ) ) ;
+	if( s == NULL ) create_topo( net ) ;
+	else{
+		fscanf( s , "%i" , &( *net )->inputs ) ;
+		fscanf( s , "%i" , &( *net )->layers ) ;
+		( *net )->topo= malloc( ( *net )->layers * sizeof( int ) ) ;
+		for( int i= 0 ; i < ( *net )->layers ; i++ ) fscanf( s , "%i" , &( *net )->topo[i] ) ;
+		( *net )->in= malloc( ( *net )->inputs * sizeof( float ) ) ;
+		( *net )->neuron= malloc( ( *net )->layers * sizeof( struct perceptron_t * ) ) ;
+		for( int i= 0 , insize= ( *net )->inputs ; i < ( *net )->layers ; i++ ) {
+			fscanf( s , "%i" , &( *net )->topo[i] ) ;
+			( *net )->neuron[i]= malloc( ( *net )->topo[i] * sizeof( struct perceptron_t ) ) ;
+			for( int j= 0 ; j < ( *net )->topo[i] ; j++ ){
+				( *net )->neuron[i][j].inputs= insize ;
+				( *net )->neuron[i][j].in= malloc( ( *net )->neuron[i][j].inputs * sizeof( float * ) ) ;
+				for( int k= 0 ; k < ( *net )->neuron[i][j].inputs ; k++ ) ( *net )->neuron[i][j].in[k]= i ? &( *net )->neuron[i - 1][k].out : &( *net )->in[k] ;
+				( *net )->neuron[i][j].weight= malloc( ( *net )->neuron[i][j].inputs * sizeof( float ) ) ;
+			}
+			insize= ( *net )->topo[i] ;
 		}
-		insize= ( *net )->topo[i] ;
+		fclose( s ) ;
 	}
 }
 void load_neurons( char *file , struct net_t ** net ){
 	FILE *s ;
 	s= fopen( file , "r" ) ;
-	for( int i= 0 ; i < ( *net )->layers ; i++ ) for( int j= 0 ; j < ( *net )->topo[i] ; j++ ){
-		fscanf( s , "%i" , &( *net )->neuron[i][j].function ) ;
-		( *net )->neuron[i][j].fun[0]= function[( *net )->neuron[i][j].function][0] ;
-		( *net )->neuron[i][j].fun[1]= function[( *net )->neuron[i][j].function][1] ;
-		fscanf( s , "%f" , &( *net )->neuron[i][j].bias ) ;
-		for( int k= 0 ; k < ( *net )->neuron[i][j].inputs ; k++ ) fscanf( s , "%f" , &( *net )->neuron[i][j].weight[k] ) ;
+	if( s == NULL ){
+		for( int i= 0 ; i < ( *net )->layers ; i++ ) for( int j= 0 ; j < ( *net )->topo[i] ; j++ ) {
+			( *net )->neuron[i][j].function= 1 ;
+			( *net )->neuron[i][j].fun[0]= function[1][0] ;
+			( *net )->neuron[i][j].fun[1]= function[1][1] ;
+		}
+		randomize_net( net , 1 , -1 ) ;
+	} else{
+		for( int i= 0 ; i < ( *net )->layers ; i++ ) for( int j= 0 ; j < ( *net )->topo[i] ; j++ ){
+			fscanf( s , "%i" , &( *net )->neuron[i][j].function ) ;
+			( *net )->neuron[i][j].fun[0]= function[( *net )->neuron[i][j].function][0] ;
+			( *net )->neuron[i][j].fun[1]= function[( *net )->neuron[i][j].function][1] ;
+			fscanf( s , "%f" , &( *net )->neuron[i][j].bias ) ;
+			for( int k= 0 ; k < ( *net )->neuron[i][j].inputs ; k++ ) fscanf( s , "%f" , &( *net )->neuron[i][j].weight[k] ) ;
+		}
+		fclose( s ) ;
 	}
-	fclose( s ) ;	
 }
 void save_neurons( char *file , struct net_t *net ){
 	FILE *f ;
